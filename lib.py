07d83dd987814323b21d0c5d282fa7a4f52d3827
Ivan <you@example.com>
2018-05-11 18:42:59 +0300

    First commit

---
+++ lib.py
@@ -0,0 +1,210 @@
+
+
+
+def getVglucosetransporter(arg):
+
+    glc_diff = arg["glc_ext"] - arg["glc_cyt"]
+    glc_cyt_Km_ratio = arg["glc_cyt"]/arg["glc_trs"]["Km_glc_cyt"]
+    glc_ext_Km_ratio = arg["glc_ext"]/arg["glc_trs"]["Km_glc_ext"]
+    V = arg["glc_trs"]["Vmax"] * glc_diff / (1 + glc_cyt_Km_ratio + glc_ext_Km_ratio)
+
+    return V
+
+def getVhexokinase(arg):
+
+    Vglc = arg["glc_cyt"] / (arg["glc_cyt"] + arg["hexokinase"]["Km_glc"])
+    Vatp = arg["atp_cyt"] / (arg["atp_cyt"] + arg["hexokinase"]["Km_atp"]*(1 + arg["glc6p"]/arg["hexokinase"]["Kiatp_glc6p"]) )
+    Inh = 1 - arg["glc6p"]/ arg["hexokinase"]["Ki_glc6p"]
+    V = arg["hexokinase"]["Vmax"] * Vglc * Vatp * Inh
+
+    return V
+
+def getVglucose6p_isomerase(arg):
+
+    tmp1 = arg["glc6p"] - arg["glc6p"] / arg["glc6p_isomerase"]["Keq_g6piso"]
+    tmp2 = 1 + arg["glc6p"]/arg["glc6p_isomerase"]["Km_glc6p"] + arg["fru6p"] / arg["glc6p_isomerase"]["Km_fru6p"]
+    V = arg["glc6p_isomerase"]["Vmax"] * tmp1 / tmp2
+    return V
+
+def getVphosphofructokinase1(arg):
+
+    fru__n = arg["fru6p"]**arg["phosphofructokinase1"]["nfru26p"]
+    tmp1 = fru__n / (fru__n + arg["phosphofructokinase1"]["Ka_fru26p"]**arg["phosphofructokinase1"]["nfru26p"] )
+    Vfru = arg["fru6p"] / (arg["fru6p"] + arg["phosphofructokinase1"]["Km_fru6p"]*(1 - arg["phosphofructokinase1"]["K0"]*tmp1) )
+    Vatp = arg["atp_cyt"] / (arg["atp_cyt"] + arg["phosphofructokinase1"]["Km_atp"])
+    Vatp__n = 1 - arg["atp_cyt"]**arg["phosphofructokinase1"]["n"]/(arg["atp_cyt"]**arg["phosphofructokinase1"]["n"] + arg["phosphofructokinase1"]["Ki_atp"]**arg["phosphofructokinase1"]["n"])
+    Vfru26p = arg["fru26p"] / (arg["fru26p"] + arg["phosphofructokinase1"]["Ka_fru26p"])
+    V = arg["phosphofructokinase1"]["Vmax"] * Vfru * Vatp * Vatp__n * Vfru26p
+
+    return V
+
+def getVfru16bisphosphatase(arg):
+
+    V = arg["fru-1,6-bisphosphatase"]["Vmax"]*arg["fru16bp"] / (arg["fru16bp"] + arg["fru-1,6-bisphosphatase"]["Km_fru6p"])
+
+    return V
+
+
+def getVphosphofructokinase2(arg):
+
+    Vfru = arg["fru6p"] / (arg["fru6p"] + arg["phosphofructokinase2"]["Km_fru6p"])
+    Vatp = arg["atp_cyt"] / (arg["atp_cyt"] + arg["phosphofructokinase2"]["Km_atp"])
+    Vamp =  arg["amp_cyt"] / (arg["amp_cyt"] + arg["phosphofructokinase2"]["Ka_amp"])
+    Vadp = arg["adp_cyt"] / (arg["adp_cyt"] + arg["phosphofructokinase2"]["Ka_adp"])
+
+    V = Vfru * Vatp * Vamp * Vadp
+    return V
+
+
+def getVfru26bisphosphatase(arg):
+
+    tmp = 1 + arg["fru6p"] / arg["fru-2,6-bisphosphatase"]["Ki_fru6p"]
+    V = arg["fru-2,6-bisphosphatase"]["Vmax"] * arg["fru26p"] / (arg["fru26p"] + arg["fru-2,6-bisphosphatase"]["Km_fru26p"]*tmp)
+    return V
+
+def getValdolase(arg):
+
+    tmp1 = arg["fru16p"] - arg["grap"]*arg["dhap"]/arg["aldolase"]["Keq_aldo"]
+    tmp2 = 1 + arg["fru16p"]/arg["aldolase"]["Km_fru16p"]
+    tmp3 = 1 + arg["grap"]/arg["aldolase"]["Km_grap"]
+    tmp4 = 1 + arg["dhap"] / arg["aldolase"]["Km_dhap"]
+
+    V = arg["aldolase"]["Vmax"] * tmp1 / (tmp2 + tmp3*tmp4 - 1)
+
+    return V
+
+def getVtriosep_isomerase(arg):
+
+    tmp1 = arg["dhap"] - arg["dhap"]/arg["triosep-isomerase"]["Keq_tri"]
+    tmp2 = 1 + arg["dhap"] / arg["triosep-isomerase"]["Km_dhap"] + arg["grap"]/arg["triosep-isomerase"]["Km_grap"]
+
+    V = arg["triosep-isomerase"]["Vmax"] * tmp1 * tmp2
+
+    return V
+
+def getVgrap_dehydrogenase(arg):
+
+    tmp1 = arg["nad_cyt"]*arg["grap"]*arg["pi"] - arg["bpg13"]*arg["nadh_cyt"]/arg["grap_dehydr"]["Keq_gapdh"]
+
+    tmp2 = 1 + arg["nad_cyt"]/arg["grap_dehydr"]["Km_nad"]
+    tmp3 = 1 + arg["grap"] / arg["grap_dehydr"]["Km_grap"]
+    tmp4 = 1 + arg["pi"] / arg["grap_dehydr"]["Km_pi"]
+
+    tmp5 = 1 + arg["nadh_cyt"] / arg["grap_dehydr"]["Km_nadh"]
+    tmp6 = 1 + arg["bpg13"] / arg["grap_dehydr"]["Km_bpg13"]
+
+    V = arg["grap_dehydr"]["Vmax"]*tmp1/(tmp2*tmp3*tmp4 + tmp5*tmp6 - 1)
+
+    return V
+
+def getVphosphoglyceratekinase(arg):
+
+    tmp1 = arg["bpg13"]*arg["adp_cyt"] - arg["pg3"]*arg["atp_cyt"]/arg["p-glyceratekinase"]["Keq_pgk"]
+    tmp2 = 1 + arg["bpg13"] / arg["p-glyceratekinase"]["Km_bpg13"]
+    tmp3 = 1 + arg["adp_cyt"] / arg["p-glyceratekinase"]["Km_adp"]
+    tmp4 = 1 +  arg["pg3"] / arg["p-glyceratekinase"]["Km_pg3"]
+    tmp5 = 1 + arg["atp_cyt"] / arg["p-glyceratekinase"]["Km_atp"]
+
+    V = arg["p-glyceratekinase"]["Vmax"] * tmp1 / (tmp2*tmp3 + tmp4*tmp5 - 1)
+
+    return V
+
+
+def getVphosphoglyceratemutase(arg):
+
+    tmp1 = arg["pg3"] - arg["pg2"]/arg["p-gricerate_mutase"]["Keq_pgm"]
+    tmp2 = 1 + arg["pg3"]/arg["p-gricerate_mutase"]["Km_pg3"]
+    tmp3 = 1 + arg["pg2"] / arg["p-gricerate_mutase"]["Km_pg2"]
+
+    V = arg["p-gricerate_mutase"]["Vmax"] * tmp1 / (tmp2 + tmp3 - 1)
+
+    return V
+
+def getVenolase(arg):
+
+    tmp1 = arg["pg2"] - arg["pep"]/ arg["enolase"]["Keq_eno"]
+    tmp2 = 1 + arg["pg2"] / arg["enolase"]["Km_pg2"]
+    tmp3 = 1 + arg["pep"] / arg["enolase"]["Km_pep"]
+
+    V = arg["enolase"]["Vmax"] * tmp1 / (tmp2 + tmp3 - 1)
+
+    return V
+
+def getVpyruvatekinase(arg):
+    Vpep = arg["pep"] / (arg["pep"] + arg["pyruvatekinase"]["Km_pep"] )
+    tmp = 1 + arg["atp_cyt"]/arg["pyruvatekinase"]["Ki_atp"]
+    Vadp = arg["adp_cyt"] / (arg["adp_cyt"] + arg["pyruvatekinase"]["Km_adp"] * tmp)
+
+    V = arg["pyruvatekinase"]["Vmax"] * Vpep * Vadp
+
+    return V
+
+def getVlactatedehydrogenase(arg):
+
+    tmp1 = arg["pyr_cyt"] * arg["nadh_cyt"] - arg["lac"]*arg["nad_cyt"]/arg["LDG"]["Keq_ldg"]
+    tmp2 = 1 + arg["pyr_cyt"] / arg["LDG"]["Km_pyr"]
+    tmp3 = 1 + arg["nadh_cyt"] / arg["LDG"]["Km_nadh"]
+    tmp4 = 1 + arg["lac"] / arg["LDG"]["Km_lac"]
+    tmp5 = 1 + arg["nad_cyt"] / arg["LDG"]["Km_nad"]
+
+    V = arg["LDG"]["Vmax"] * tmp1 / (tmp2*tmp3 + tmp4*tmp5 - 1)
+
+    return V
+
+def getVmonocarboxilatetransporter(arg):
+
+    tmp1 = arg["lac"] - arg["lac_ext"] / arg["MCT"]["Keq_mct"]
+    tmp2 = 1 + arg["lac"] / arg["MCT"]["Km_lac"]
+    tmp3 = 1 + arg["lac_ext"] / arg["MCT"]["Km_lac_ex"]
+
+    V = arg["MCT"]["Vmax"] * tmp1 / (tmp2 + tmp3 - 1)
+
+    return V
+
+def getVcreatinekinase(arg):
+
+    tmp = arg["atp_cyt"]*arg["cr"] / arg["creatinekinase"]["Keq"]
+    V = arg["creatinekinase"]["Vmax"]*(arg["adp_cyt"]*arg["crp"] - tmp)
+
+    return V
+
+def getVmalatdehydrogenase(arg, mode="mit"):
+
+    if mode == "cyt":
+        mal = arg["mal_cyt"]
+        oa = arg["oa_cyt"]
+        nad = arg["nad_cyt"]
+        nadh = arg["nadh_cyt"]
+    elif mode == "mito":
+        mal = arg["mal_mit"]
+        oa = arg["oa_mit"]
+        nad = arg["nad_mit"]
+        nadh = arg["nadh_mit"]
+
+    tmp1 = mal*nad - oa*nadh/arg["malatdehyd"]["Keq"]
+    tmp2 = 1 + mal / arg["malatdehyd"]["Km_mal"]
+    tmp3 = 1 + nad / arg["malatdehyd"]["Km_nad"]
+    tmp4 = 1 + oa / arg["malatdehyd"]["Km_oa"]
+    tmp5 = 1 + nadh / arg["malatdehyd"]["Km_nadh"]
+
+    V = arg["malatdehyd"]["Vmax"] * tmp1 / (tmp2*tmp3 + tmp4*tmp5 - 1)
+
+    return V
+
+def getVaspartateaminotransferase(arg, mode="mit"):
+
+    if mode == "cyt":
+        asp = arg["asp_cyt"]
+        oa = arg["oa_cyt"]
+        akg = arg["nad_cyt"]
+        glu = arg["nadh_cyt"]
+    elif mode == "mito":
+        asp = arg["asp_mit"]
+        oa = arg["oa_mit"]
+        akg = arg["nad_mit"]
+        glu = arg["nadh_mit"]
+
+    tmp = oa * glu / arg["asp_aminotrans"]["Keq"]
+    V = arg["asp_aminotrans"]["Vmax"] * (asp * akg - tmp)
+
+    return V
---
+++ simulation.py
@@ -0,0 +1,250 @@
+import numpy as np
+from scipy.integrate import ode
+import lib
+
+y0, t0 = [1.0], 0
+
+# Notations
+# glc is glucose
+# atp ia ATP
+# glc6p is glucose-6-P
+# fru6p is fructose-6-P
+
+
+arg = {
+
+    "glc_ext" : 0,
+    "glc_cyt" : 0,
+    "atp_cyt" : 0,
+    "adp_cyt" : 0,
+    "amp_cyt" : 0,
+    "glc6p"   : 0,
+    "fru6p"   : 0,
+    "fru26p"  : 0,
+    "fru16bp" : 0,
+    "fru16p"  : 0,
+    "pi"      : 0,
+    "grap"    : 0,
+    "dhap"    : 0,
+    "bpg13"   : 0,
+    "pg3"     : 0,
+    "pg2"     : 0,
+    "pep"     : 0,
+    "pyr_cyt" : 0,
+    "lac"     : 0,
+    "lac_ext" : 0,
+    "cr"      : 0,
+    "crp"     : 0,
+
+    "mal_cyt" : 0,
+    "oa_cyt"  : 0,
+    "mal_mit":  0,
+    "oa_mit":   0,
+    "asp_cyt" : 0,
+    "asp_mit" : 0,
+    "akg_cyt" : 0,
+    "akg_mit" : 0,
+    "glu_cyt" : 0,
+    "glu_mit" : 0,
+
+    "nad_cyt" : 0,
+    "nadh_cyt": 0,
+    "nad_mit" : 0,
+    "nadh_mit": 0,
+
+    "asp_aminotrans" : {
+        "Vmax" : 32,
+        "Keq"  : 0.147,
+
+    },
+
+    "malatdehyd" : {
+        "Vmax" : 10**4,
+        "Keq"  : 10**-4,
+        "Km_nad" : 0.05,
+        "Km_mal" : 0.77,
+        "Km_oa"  : 0.04,
+        "Km_nadh" : 0.05,
+    },
+
+    "creatinekinase" : {
+        "Vmax" : 0.0135,
+        "Keq"  : 7.0
+    },
+
+    "MCT" : {
+        "Vmax" : 5.0,
+        "Keq_mct" : 1.737,
+        "Km_lac" : 1.1,
+        "Km_lac_ex" : 1.1,
+    },
+
+    "LDG" : {
+        "Vmax" : 10**5,
+        "Keq_ldg" : 8400,
+        "Km_pyr"  : 0.36,
+        "Km_nadh" : 0.043,
+        "Km_lac"  : 4.2,
+        "Km_nad"  : 0.088,
+    },
+
+    "pyruvatekinase": {
+        "Vmax" : 23.76,
+        "Km_pep" : 0.074,
+        "Km_adp" : 0.42,
+        "Ki_atp" : 4.4,
+    },
+
+    "enolase": {
+        "Vmax" : 216000,
+        "Keq_eno" : 0.5,
+        "Km_pg2"  : 0.05,
+        "Km_pep"  : 0.15,
+    },
+
+    "p-gricerate_mutase": {
+        "Vmax" : 14400,
+        "Keq_pgm" : 0.1814,
+        "Km_pg3"  : 0.22,
+        "Km_pg2"  : 0.28,
+
+    },
+
+    "p-glyceratekinase": {
+        "Vmax" : 396,
+        "Keq_pgk" : 1310,
+        "Km_bpg13" : 0.063,
+        "Km_adp"   : 0.42,
+        "Km_pg3"   : 0.67,
+        "Km_atp"   : 0.25,
+    },
+
+    "grap_dehydr" : {
+        "Vmax" : 72000,
+        "Keq_gapdh" : 0.0868,
+        "Km_nad"    : 0.01, # 0.027
+        "Km_grap"   : 0.101,
+        "Km_pi"     : 3.9,
+        "Km_nadh"   : 0.008,
+        "Km_bpg13"  : 0.0035,
+
+
+    },
+
+    "triosep-isomerase": {
+        "Vmax" : 10**6,
+        "Keq_tri": 0.0545,
+        "Km_dhap" : 0.84,
+        "Km_grap" : 1.65,
+    },
+
+    "aldolase" : {
+        "Vmax" : 46.8,
+        "Keq_aldo" : 0.0976,
+        "Km_fru16p" : 0.003,
+        "Km_grap"   : 0.08,
+        "Km_dhap"   : 0.03,
+
+    },
+
+    "fru-2,6-bisphosphatase" : {
+        "Vmax" : 0.052,
+        "Km_fru26p" : 0.07,
+        "Ki_fru6p" : 0.02,
+
+    },
+
+    "phosphofructokinase2": {
+        "Vmax" : 0.0026,
+        "Km_fru6p" : 0.027,
+        "Km_atp"   : 0.055,
+        "Ka_amp"   : 0.073,
+        "Ka_adp"   : 0.056,
+
+    },
+
+    "fru-1,6-bisphosphatase" : {
+        "Vmax" : 0.455,
+        "Km_fru6p" : 0.132,
+    },
+
+    "phosphofructokinase1": {
+        "Vmax" : 49.6,
+        "Km_fru6p" : 0.111,
+        "Km_atp"   : 0.04,
+        "n" : 1.8,
+        "Ki_atp" : 1.2,
+        "K0" : 0.55,
+        "Ka_fru26p" : 0.0042,
+        "nfru26p" : 5.5,
+        "Ka_fru26p" : 0.005,
+
+    },
+
+    "glc6p_isomerase" : {
+        "Vmax" : 24.4,
+        "Keq_g6piso": 0.5157,
+        "Km_glc6p" : 0.593,
+        "Km_fru6p": 0.095,
+    },
+
+    "hexokinase" : {
+        "Vmax" : 9.36,
+        "Km_glc" : 0.043,
+        "Km_atp" : 0.37,
+        "Kiatp_glc6p" : 0.074,
+        "Ki_glc6p" : 0.1,
+
+
+    },
+
+    "glc_trs" : {
+        "Vmax" : 0.72,
+        "Km_glc_cyt" : 2.87,
+        "Km_glc_ext" : 2.87,
+    }
+
+
+}
+
+
+def model_equations(t, y, arg):
+
+    # Glycolysis
+
+    # Glucose transporter
+    vglc_transp = lib.getVglucosetransporter(arg)
+    # Hexokinase
+    vhexokinase = lib.getVhexokinase(arg)
+    glucose6p_isomerase = lib.getVglucose6p_isomerase(arg)
+    phosphofructokinase1 = lib.getVphosphofructokinase1(arg)
+    fru16bisphosphatase = lib.getVfru16bisphosphatase(arg)
+    aldolase = lib.getValdolase(arg)
+    triosep_isomerase = lib.getVtriosep_isomerase(arg)
+    grap_dehydr = lib.getVgrap_dehydrogenase(arg)
+    ph_glyceratekinase = lib.getVphosphoglyceratekinase(arg)
+    ph_glyceratemutase = lib.getVphosphoglyceratemutase(arg)
+    enolase = lib.getVenolase(arg)
+    pyruvatekinase = lib.getVpyruvatekinase(arg)
+    ldg = lib.getVlactatedehydrogenase(arg)
+    # Monocarboxilate transporter
+    mct = lib.getVmonocarboxilatetransporter(arg)
+    creatinekinase = lib.getVcreatinekinase(arg)
+    cyt_malatdehydrogenase = lib.getVmalatdehydrogenase(arg, mode="cyt")
+    mito_malatdehydrogenase = lib.getVmalatdehydrogenase(arg, mode="mit")
+
+    cyt_asp_aminotrans = lib.getVaspartateaminotransferase(arg, mode="mit")
+    mito_asp_aminotrans = lib.getVaspartateaminotransferase(arg, mode="mit")
+
+    return [vglc_transp, vhexokinase, glucose6p_isomerase]
+
+model_equations(0, 0, arg)
+
+# r = ode(model_equations).set_integrator('dopri5', method='bdf')
+#
+# r.set_initial_value(y0, t0).set_f_params([0.2])  #.set_jac_params([-0.2])
+# t1 = 10
+# dt = 0.1
+# while r.successful() and r.t < t1:
+#     # r.integrate(r.t + dt)
+#     print(r.t+dt, r.integrate(r.t+dt))
